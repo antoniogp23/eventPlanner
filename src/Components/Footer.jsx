fa07f840726a00208f443429d7e121cfcb5f9ee5
antoniogp23 <gutierrezantonio2397@gmail.com>
2024-09-19 14:14:18 -0400

    Primer commit de mi proyecto React

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+    import React from 'react';
+    const Footer = () => {
+            return (
+                <footer>
+                    <div>
+                        <p>
+                        @Event Planner Antonio Gutierrez. All rights reserved.
+                        </p>
+                    </div>
+                    
+                </footer>
+
+    )}
+
+export default Footer
